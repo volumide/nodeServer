9faec037830fb73a5ca259be9fa9a593169a4733
Olumide <volumide42@gmai.com>
2020-07-24 10:08:41 +0100

    chnage message

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let http = require('http')
 http.createServer( (req, res) => {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World, welcome to WeJapa Internships')
-}).listen(8080, 'localhost')
-console.log('server running at http://localhost:8080')
+	res.end('Hello World, welcome to WeJapa Internship')
+}).listen(8081, 'localhost')
+console.log('server running at http://localhost:8081')
