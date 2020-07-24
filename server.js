fd4fc5df461aa2c65ce390c88b9396e6fdf8c7d8
Olumide <volumide42@gmai.com>
2020-07-24 10:03:19 +0100

    change write ti writeHead

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 let http = require('http')
 http.createServer( (req, res) => {
-	res.write('Hello World, welcome to WeJapa Internships');
-	res.end()
-}).listen(8080)
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.end('Hello World, welcome to WeJapa Internships')
+}).listen(8080, 'localhost')
+console.log('server running at http://localhost:8080')
